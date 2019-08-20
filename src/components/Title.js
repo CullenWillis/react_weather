e929fdfc779dbaeec943c5050ed2eba4bbde7c2f
Cullen Willis <cullenwillis96@gmail.com>
2019-08-20 21:49:44 +0100

    final

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = () => (
+  <div>
+    <h1 className="title-container__title">Weather Finder </h1>
+    <h3 className="title-subtitle">
+      Find out the tempature, conditions and more...{" "}
+    </h3>
+  </div>
+);
+
+export default Title;
